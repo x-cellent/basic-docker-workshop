3a12571f96a8ed6922ac236171ff84b2412984bd
Sandro Koll <sandro.koll@f-i-ts.de>
2020-01-14 09:34:44 +0100

    Some adaptions

--- 2-solution-linting/countdown/src/main/java/workshop/Countdown.java
+++ 2-solution-linting/countdown/src/main/java/workshop/Countdown.java
@@ -3,7 +3,7 @@ package workshop;
 public class Countdown {
 
     public static void main(String[] args) {
-        for (int i = 10; i >= 0; i--) {
+        for (int i = 20; i >= 0; i--) {
             System.out.println(i);
             if (i > 0) {
                 try {
