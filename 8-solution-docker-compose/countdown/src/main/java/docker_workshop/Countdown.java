299603244a5a06bc2f8b050ca04f01245ea9a998
Sandro Koll <sandro.koll@f-i-ts.de>
2020-01-15 11:24:42 +0100

    Update Praesentation.pdf

--- 8-solution-docker-compose/countdown/src/main/java/docker_workshop/Countdown.java
+++ 8-solution-docker-compose/countdown/src/main/java/docker_workshop/Countdown.java
@@ -3,7 +3,7 @@ package docker_workshop;
 public class Countdown {
 
     public static void main(String[] args) {
-        for (int i = 20; i >= 0; i--) {
+        for (int i = 10; i >= 0; i--) {
             System.out.println(i);
             if (i > 0) {
                 try {
